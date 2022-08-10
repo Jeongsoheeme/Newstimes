8294fdf324bd396d9f67b10b5bbdd1aace2e0347
Jeongsohee <toy3045@gmail.com>
2022-08-11 00:37:53 +0900

    api

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+let news = [];
 
-
-const getLatestNews =async()=>{
-    let url = new URL(`https://api.newscatcherapi.com/v2/search?q=Apple&from='2022/07/19' &countries=KR&page_size=10`);
-    let header = new Headers({'x-api-key':'C2DcVWEX9pANNudnpY7oGXXtfK7spQ3yXQQQdEf4mpA'});
-
-    let response = await fetch(url,{headers: header });
-    console.log(response);
-
+const getLatestNews = async()=>{
+    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10`);
+    let header = new Headers({'x-api-key':'C2DcVWEX9pANNudnpY7oGXXtfK7spQ3yXQQQdEf4mpA',});
+    
+    // 데이터보내는방식 3가지 : ajax, http, fetch
+    //async(비동기)와 await는 세트!
+    let response = await fetch(url,{headers:header}); 
+    let data = await response.json();
+    news = data.articles
+    console.log(news);
 };
 
 getLatestNews();
